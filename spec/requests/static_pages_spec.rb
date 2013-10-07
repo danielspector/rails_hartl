eb6f6099a23e05356a7b44f931f7b68d4a3a9e7f
danielspector <danielyspector@gmail.com>
2013-10-07 01:40:06 -0400

    Finish static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,14 +8,36 @@ describe "StaticPages" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
     	visit '/static_pages/home'
       	expect(page).to have_content('Sample App')
+     end
+
+     it "should have the title 'Home' " do
+     	visit '/static_pages/home'
+     	expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
     end
   end
 
   describe "Help Page" do
 
-  	it "should have the content 'Help' " do
+  	it "should have the content 'help' " do
   		visit '/static_pages/help'
-  		expect(page).to have_content('Help')
+  		expect(page).to have_content('help')
+  	end
+  	
+  	it "should have the title 'Help' " do
+  		visit '/static_pages/help'
+  		expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
   	end
   end
+
+  describe "About Page" do
+  	it "should have the content 'about'" do
+  		visit '/static_pages/about'
+  		expect(page).to have_content('about')
+  	end
+
+  	it "should have the title 'About' " do
+  	visit '/static_pages/about'
+  	expect(page).to have_title("Ruby on Rails Tutorial Sample App | About") 
+  	end
+	end
 end
